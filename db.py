196d70d632be4f5059fb7437c0c7c8e57326510c
grovessss <60423593+grovessss@users.noreply.github.com>
2022-04-20 00:10:32 +0800

    parse data & add uuid in db

--- db.py
+++ db.py
@@ -6,15 +6,15 @@ DATABASE_PATH = ""
 def create_db():
     con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
-    # currently store mac as string
-    cur.execute("CREATE TABLE data (id integer primary key, src text, mac text, timestamp BIGINT, rssi integer)")
+    # currently store mac as string  # add uuid text
+    cur.execute("CREATE TABLE data (id integer primary key, src text, mac text, uuid text, timestamp BIGINT, rssi integer)")
     con.commit()
     con.close()
 
-def insert(src: str, mac: str, timestamp: int, rssi: int):
+def insert(src: str, mac: str, uuid: str, timestamp: int, rssi: int):
     con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
-    cmd = "INSERT INTO data (src, mac, timestamp, rssi) VALUES ('%s', '%s', %d, %d)" % (src, mac, timestamp, rssi)
+    cmd = "INSERT INTO data (src, mac, uuid, timestamp, rssi) VALUES ('%s', '%s', '%s', %d, %d)" % (src, mac, uuid, timestamp, rssi)
     cur.execute(cmd)
     con.commit()
     con.close()
--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 import db
-
 import os
 from flask import Flask, render_template, request
-import json
+import json, msgpack
+from beacontools import parse_packet
+from beacontools.packet_types.ibeacon import IBeaconAdvertisement
+
+run_on = '172.20.10.2' # configure gateway to send to this address
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<p>Index Page</p>"
-
-@app.route("/upload")
-def upload():
-    return render_template("upload.html")
-
-@app.route("/uploader", methods = ['POST'])
+@app.route("/", methods = ['POST'])
 def uploader():
-    f = request.files['file']
-    tmp_filename = os.path.join('tmp', '1.tmp')
-    f.save(tmp_filename)
-
-    data = {}
-    with open(tmp_filename) as f:
-        data = json.loads(f.read())
-    raw_beacons_data = data['raw_beacons_data']
-
-    src = 'receiver1'
-    mac = raw_beacons_data[:12]
+    data = msgpack.loads(request.get_data()) # data keys: dict_keys(['v', 'mid', 'time', 'ip', 'mac', 'devices'])
+    devices = data['devices']  # devices is list of bytes
+    src = data['mac']          # mac is a str(gateway mac)
     timestamp = 12345
-    rssi = int(raw_beacons_data[56:58]) # TODO: find out whether it is hex
-    db.insert(src, mac, timestamp, rssi)
 
+    for device in devices:
+        mac = "{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}".format((device[1]), 
+            (device[2]), (device[3]), (device[4]), (device[5]), (device[6]))
+        rssi = device[7] - 256
+        adv = parse_packet(device[8:])
+        
+        # devices are of various length, parse_packet only parse len(device[8:])==30?
+        if type(adv) == IBeaconAdvertisement:
+            db.insert(src, mac, adv.uuid, timestamp, rssi)
     return "uploaded"
+
 
 @app.route("/time")
 def timer():
@@ -42,7 +37,8 @@ def main() -> None:
     if not os.path.exists("tmp/"):
         os.mkdir("tmp/")
     db.init()
-    app.run()
+    app.run(run_on)
 
 if __name__ == "__main__":
     main()
+
